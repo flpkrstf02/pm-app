ebee404b70cf1ba7637497af5ba01f21d8abd623
Kristof Fulop <f0kristof@outlook.com>
2023-12-02 17:05:57 +0100

    refact(app.module): remove unnecessary componens

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AkosComponent } from './akos/akos.component';
-import { MartinComponent } from './martin/martin.component';
-import { VajkComponent } from './vajk/vajk.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'akos-component', component: AkosComponent },
-  { path: 'martin-component', component: MartinComponent },
-  { path: 'vajk-component', component: VajkComponent },
+  { path: '', component: HomeComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AkosComponent } from './akos/akos.component';
 import { HomeComponent } from './home/home.component';
-import { VajkComponent } from './vajk/vajk.component';
-import { MartinComponent } from './martin/martin.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
 import { ModelComponent } from './model/model.component';
@@ -19,10 +16,7 @@ import { ModelComponent } from './model/model.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    AkosComponent,
     HomeComponent,
-    VajkComponent,
-    MartinComponent,
     CanvasJSAngularChartsModule,
     ModelComponent
   ],
